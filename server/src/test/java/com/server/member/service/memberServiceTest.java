ddb218c01f5c8569a785a8bb00a86e08225c8775
KimSoHee <118681718+BillionDollarSohee@users.noreply.github.com>
2023-08-23 13:17:28 +0900

    프로덕션 코드 추가

---
+++ server/src/test/java/com/server/member/service/memberServiceTest.java
@@ -0,0 +1,14 @@
+package com.server.member.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class memberServiceTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+}
